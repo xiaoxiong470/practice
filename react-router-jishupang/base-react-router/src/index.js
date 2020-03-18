23f00d6ce70c6025f30a34d509cdc61cadcca836
xxy <15931185617@163.com>
2020-03-18 22:13:00 +0800

    delete

--- react-router-jishupang/base-react-router/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppRouter from "./AppRouter";
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
-
--- reactrefdemo/src/FancyButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logdProps from "./logProps";
-class FancyButton extends React.Component {
-    render() {
-        return (<div>hello</div>)
-    }
-}
-export default logdProps(FancyButton);
