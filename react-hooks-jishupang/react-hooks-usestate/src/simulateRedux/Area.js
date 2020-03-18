24e99bb56a9f459d1f3d80817ed581ba9fffc632
xxy <15931185617@163.com>
2020-03-18 22:11:06 +0800

    删除

--- react-hooks-jishupang/react-hooks-usestate/src/simulateRedux/Area.js
+++
@@ -1,8 +0,0 @@
-import React,{useContext} from "react";
-import {ColorContext} from "./Color";
-
-export function Area() {
-    let obj=useContext(ColorContext);
-    console.log(obj);
-    return (<div style={{color:obj.color}}>hello,world</div>);
-}
